3425334a675645284314ed98752a0175d71956f6
root <root@Hiep>
2024-11-20 14:10:05 +0700

    fix deploy

--- src/components/FieldPlugin/Filerobot.vue
+++ src/components/FieldPlugin/Filerobot.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import { useFieldPlugin } from '@storyblok/field-plugin/vue3'
-
-import Filerobot from '@filerobot/core'
-import Explorer from '@filerobot/explorer'
-import XHRUpload from '@filerobot/xhr-upload'
-import '@filerobot/core/dist/style.min.css'
-import '@filerobot/explorer/dist/style.min.css'
-import { ref, watch } from 'vue'
+import { watch, onMounted } from 'vue'
+import { loadScript } from "vue-plugin-load-script";
 
 interface SelectedFiles {
   (files: any[]): void; // Adjust the type of 'files' based on its structure
@@ -20,21 +15,39 @@ const plugin = useFieldPlugin({
   enablePortalModal: true,
 })
 
+onMounted(() => {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://cdn.scaleflex.com/plugins/filerobot-widget/v3/latest/filerobot-widget.min.css';
+  document.head.appendChild(link);
+})
+
 watch(() => plugin.data, (newPlugin) => {
     if (newPlugin && newPlugin.isModalOpen) {
+    loadScript("https://cdn.scaleflex.com/plugins/filerobot-widget/v3/latest/filerobot-widget.min.js")
+    .then(() => {
+      // Script is loaded, do something
         let limitTypeArr: any = []
         if ('limitType' in newPlugin.options && newPlugin.options.limitType && newPlugin.options.limitType != '') {
           limitTypeArr = newPlugin.options.limitType.split(",").map(function(item) {
             return item.trim();
           })
         }
-        const demoContainer = newPlugin.options.token;
-        const demoSecurityTemplateID = newPlugin.options.secTemplate;
+        const container = newPlugin.options.token;
+        const securityTemplateID = newPlugin.options.secTemplate;
         const rootFolderPath = newPlugin.options.rootDir ?? '/';
-        const filerobot = Filerobot({
-            securityTemplateId: demoSecurityTemplateID,
-            container: demoContainer,
-            dev: false // optional, default: false
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment     
+        // @ts-ignore
+        const Filerobot = window.Filerobot;
+        const Core = Filerobot.Core; // required (docs: https://www.npmjs.com/package/@filerobot/core)
+        const Explorer = Filerobot.Explorer; // required (docs: https://www.npmjs.com/package/@filerobot/explorer)
+        const XHRUpload = Filerobot.XHRUpload; // required (docs: https://www.npmjs.com/package/@filerobot/xhr-upload)
+        const filerobot = Core({
+          securityTemplateId: securityTemplateID,
+          // sassKey:
+          //   "SASS__v1.05__wM0gTO0UTO3AzN4AjN4YTMxYDN6AXC0QTM6QWamlAMwQjN4oTZnFWCtlmbox2ZiZ2Lt92YuQ3bi9mclxWam5SawFGHvlmLlJ3b0NncpFmLpBXYu0WauhGbnJmZboDZJADMwIjM6AXatVXCwADMyIjOtBXb1lAMwAjMyoTb1lQNwIzM5MjN3YTM6Q3c__b3609fa624",
+          container: container,
+          dev: false // optional, default: false
         });
 
         filerobot
@@ -69,12 +82,18 @@ watch(() => plugin.data, (newPlugin) => {
             }
         })
         .use(XHRUpload)
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment     
+        // @ts-ignore
         .on('export', function(files) {
             // Define these functions or replace them with actual logic
             props.selectedFiles(files)
             files = []
             return false
         });
+    })
+    .catch(() => {
+      // Failed to fetch script
+    });
     }
 })
 
