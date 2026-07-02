53eb9193cfd066522369c5f83ea351ec583c1393
Vuong Do <bavuong0810@gmail.com>
2026-07-02 13:53:25 +0700

    Add CNAME configuration

--- src/components/FieldPlugin/index.vue
+++ src/components/FieldPlugin/index.vue
@@ -465,7 +465,7 @@ const log = () => {
              class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
         </svg>
-        <strong>cname</strong> is optional (CNAME for Scaleflex assets)
+        <strong>cname</strong> is optional (Custom CNAME). Check your CNAME configuration in Hub <a href="https://hub.scaleflex.com/settings/dmo/delivery/cname" target="_blank">here</a>
       </div>
       <div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
